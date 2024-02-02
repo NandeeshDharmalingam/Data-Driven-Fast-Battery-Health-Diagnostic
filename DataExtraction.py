c576406824dc098dcba7abdbb15c549e05565ced
NandeeshDharmalingam <nandeeshdharma@gmail.com>
2024-02-02 10:40:54 +0100

    NICE!!!

--- DataExtraction.py
+++ DataExtraction.py
@@ -45,13 +45,22 @@ def extract_and_save_mat_files(folder_path, columns_to_extract):
             else:
                 print(f"Skipping file '{file}' due to columns having different lengths.")
 
-# Define the columns you want to extract
-columns_to_extract = [
-    'Time', 'DataSet', 'tStep', 'Line', 'Command', 'U', 'I', 'Ah', 'AhStep', 'AhSet',
-    'AhChSet', 'AhDisSet', 'Wh', 'WhStep', 'T1', 'RAC', 'RDC', 'CycCount', 'Count',
-    'State', 'i_cut_hi', 'i_cut_lo', 'i_cut_low', 'i_cut_high'
-]
+def main():             
+    '''# Define the columns you want to extract from cyclic ageging  
+    columns_to_extract = [
+        'Time', 'DataSet', 'tStep', 'Line', 'Command', 'U', 'I', 'Ah', 'AhStep', 'AhSet',
+        'AhChSet', 'AhDisSet', 'Wh', 'WhStep', 'T1', 'RAC', 'RDC', 'CycCount', 'Count',
+        'State', 'i_cut_hi', 'i_cut_lo', 'i_cut_low', 'i_cut_high'
+    ]'''
 
-folder_path = 'E:\Thesis CEVT\Dataset\TUM\Cyc01'
-cyclepath='E:\Thesis CEVT\Dataset\TUM'
-extract_and_save_mat_files(folder_path, columns_to_extract)
+    # Define the columns you want to extract from checkup cyclic aging
+    columns_to_extract = [
+        'Time', 'DataSet', 'tStep', 'Line', 'Command', 'U', 'I', 'Ah', 'AhStep', 'AhSet',
+         'Wh',  'T1', 'RAC', 'RDC', 'CycCount', 
+        'State']
+    folder_path = 'Dataset/TUM/CU_CYC'
+    cyclepath='E:\Thesis CEVT\Dataset\TUM\CU_CYC'
+    extract_and_save_mat_files(folder_path, columns_to_extract)
+
+if __name__ == "__main__":
+    main()
--- readingMatlab.py
+++ readingMatlab.py
@@ -1,57 +1,85 @@
-import os
-import scipy.io
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
+import os
+import glob
 
-def extract_and_save_mat_files(folder_path, columns_to_extract):
-    for file in os.listdir(folder_path):
-        if file.endswith('.mat'):
-            # Construct the full file path
-            file_path = os.path.join(folder_path, file)
-            # Load the .mat file
-            mat_data = scipy.io.loadmat(file_path)
-            # Extract the 'Dataset' key
-            dataset = mat_data['Dataset']
+def collect_data_from_csvs(directory_path):
+    return glob.glob(os.path.join(directory_path, '*.csv'))
 
-            # Create a dictionary to store the extracted data
-            data = {}
+def charging_part(file_path):
+    df = pd.read_csv(file_path)
+    df['is_charging'] = df['Command'].str.contains('Charge')  # Identify charging instances
+    df['charging_phase'] = (df['is_charging'] & ~df['is_charging'].shift(1).fillna(False)).cumsum()
+    
+    # Count unique charging cycles
+    num_charging_cycles = df[df['is_charging']]['charging_phase'].nunique()
+    print(f"Number of charging cycles: {num_charging_cycles}")
+    
+    charging_data = df[df['is_charging']].copy()
+    charging_data['deltaV'] = charging_data.groupby('charging_phase')['U'].diff()
+    charging_data['deltaAh'] = charging_data.groupby('charging_phase')['Ah'].diff()
+    charging_data['dAh/dV'] = np.where(charging_data['deltaV'] != 0, charging_data['deltaAh'] / charging_data['deltaV'], np.nan)
+    ICvalues=charging_data['dAh/dV'].copy()
+    
+    plt.figure(figsize=(10, 8))
+    for phase in charging_data['charging_phase'].unique():
+        phase_data = charging_data[charging_data['charging_phase'] == phase]
+        plt.plot(phase_data['U'], phase_data['dAh/dV'], label=f'Phase {phase}')
+    
+    plt.xlabel('Voltage (U)')
+    plt.ylabel('Incremental capacity [dAh/dV]')
+    plt.title('IC curve')
+    #plt.legend()
+    plt.show()
 
-            # Initialize an array to store the lengths of fields
-            array_lengths = {}
+    return df, num_charging_cycles , ICvalues
 
-            # Iterate through the columns to extract
-            for field in columns_to_extract:
-                if isinstance(dataset[field], np.ndarray):
-                    # If the field is an array, extract and store it
-                    data[field] = dataset[field][0][0].squeeze()
-                    # Get the length of the array
-                    array_lengths[field] = len(data[field])
-                else:
-                    # If it's not an array, store it as is
-                    data[field] = dataset[field]
-                    # Set a default length of 1
-                    array_lengths[field] = 1
+def plot_charging_phase(df, phase_number):
 
-            # Check if all arrays have the same length
-            if len(set(array_lengths.values())) == 1:
-                # Convert the dictionary to a DataFrame
-                df = pd.DataFrame(data)
-                # Define the CSV file path (change 'extracted_' to any prefix you prefer)
-                csv_file_name = 'extracted_' + os.path.splitext(file)[0] + '.csv'
-                csv_file_path = os.path.join(folder_path, csv_file_name)
-                # Save the DataFrame to a CSV file
-                df.to_csv(csv_file_path, index=False)
-                print(f"Processed and saved: {csv_file_name}")
-            else:
-                print(f"Skipping file '{file}' due to columns having different lengths.")
+    phase_data = df[df['charging_phase'] == phase_number]
+    if not phase_data.empty:
+        plt.figure(figsize=(10, 5))
+        plt.plot(phase_data['Time'], phase_data['U'], label='Voltage (U)')
+        plt.plot(phase_data['Time'], phase_data['Ah'], label='Capacity (Ah)')
+        plt.xlabel('Time')
+        plt.ylabel('[U]/[Ah]')
+        plt.title(f'Voltage and Capacity during Charging Phase {phase_number}')
+        plt.legend()
+        plt.show()
+    else:
+        print(f"No data available for charging phase {phase_number}.")
 
-# Define the columns you want to extract
-columns_to_extract = [
-    'Time', 'DataSet', 'tStep', 'Line', 'Command', 'U', 'I', 'Ah', 'AhStep', 'AhSet',
-    'AhChSet', 'AhDisSet', 'Wh', 'WhStep', 'T1', 'RAC', 'RDC', 'CycCount', 'Count',
-    'State', 'i_cut_hi', 'i_cut_lo', 'i_cut_low', 'i_cut_high'
-]
+'''
 
-folder_path = 'E:\Thesis CEVT\Dataset\TUM\Cyc01'
-cyclepath='E:\Thesis CEVT\Dataset\TUM'
-extract_and_save_mat_files(folder_path, columns_to_extract)
+def plot_smoothed_ic_curve(charging_data,ICvalues):
+    plt.figure(figsize=(10,8))
+
+    for phase in charging_data['charging_phase'].unique():
+        phase_data = charging_data[charging_data['charging_phase'] == phase]
+        if len(phase_data) < 3:  # Skip if too few points for smoothing
+            continue
+        #phase_data['dAh/dV'] = phase_data['deltaAh'] / phase_data['deltaV']
+        dAh_dV_smoothed = savgol_filter(ICvalues.dropna(), window_length=51, polyorder=3)
+        plt.plot(phase_data['U'][1:-1], dAh_dV_smoothed, label=f'Phase {phase}')                       #ERROR TO BE RECTIFIED THE DIMENSION OF X AND Y IS NOT RIGHT 
+
+    plt.xlabel('Voltage (U)')
+    plt.ylabel('Smoothed Incremental Capacity (dAh/dV)')
+    plt.title('Smoothed IC Curves')
+    plt.legend()
+    plt.show()
+
+'''
+
+
+
+def main():
+    file_path = 'E:\\Thesis CEVT\\Dataset\\TUM\\Cyc01\\extracted_BW-VTC-429_3577_Cyc01_VV_20deg_BW-VTC-CYC.csv'
+    df, num_charging_cycles, ICvalues = charging_part(file_path)
+    print(f"The dataset contains {num_charging_cycles} charging cycles.")
+    phase_number = 1  
+    plot_charging_phase(df, phase_number)
+    #plot_smoothed_ic_curve(df,ICvalues)
+if __name__ == "__main__":
+    main()
